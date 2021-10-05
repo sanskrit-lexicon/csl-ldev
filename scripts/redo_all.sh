d7cc88430dc86a460663cf16c55b22ef2d778a04
drdhaval2785 <drdhaval2785@gmail.com>
2021-10-05 08:38:02 +0530

    add completion message in progress

--- scripts/redo_all.sh
+++ scripts/redo_all.sh
@@ -1,9 +1,10 @@
-echo "UPDATE CSL-ORIG REPOSITORY."
-cd ../../csl-orig
-git pull origin master
-echo "UPDATING CSL-ORIG COMPLETED."
+echo "REGENERATE CSL-DEVANAGARI REPOSITORY."
+cd ../../csl-devanagari/scripts
+bash redo_all.sh
+echo "REGENERATING CSL-DEVANAGARI COMPLETED."
 echo ""
-cd ../csl-ldev/scripts
+cd ../../csl-ldev/scripts
+echo "REGENERATE CSL-LDEV REPOSITORY."
 dicts=(wil yat gst ben mw72 lan cae md mw shs ap90 mwe bor ae bur stc pwg gra pw ccs sch bop armh vcp skd inm vei pui bhs acc krm ieg snp pe pgn mci)
 for dict in ${dicts[@]};
 do
@@ -11,3 +12,5 @@ do
 	python3 txt_to_ldev.py $dict;
 	echo "";
 done
+echo "REGENERATING CSL-LDEV COMPLETED."
+
